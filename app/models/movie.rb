5be80cb072bec589a91fc5019a09b6c0a0ce5566
kavunshiva <kavunshiva@gmail.com>
2017-04-21 15:54:33 -0400

    actually paste solutions into solutions.rb

--- app/models/movie.rb
+++ app/models/movie.rb
@@ -40,33 +40,3 @@ class Movie
     sum_of_all_scores.to_f / ratings_scores.size
   end
 end
-
-class Viewer
-  attr_accessor :first_name, :last_name
-
-  ALL = []
-
-  def self.all
-    ALL
-  end
-
-  def self.find_by_name(name)
-    self.all.find do |viewer|
-       viewer.full_name == name
-    end
-  end
-
-  def initialize(name)
-    #@name = name # not really needed, but came with the challenge
-    @first_name, @last_name = name.split
-    self.class.all << self
-  end
-
-  def full_name
-    "#{first_name} #{last_name}"
-  end
-
-  def create_rating(score, movie)
-    Rating.new(score, movie, self)
-  end
-end
