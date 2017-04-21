81b07bc2591bfc1f4ebd2a8305cecf8b63aee667
kavunshiva <kavunshiva@gmail.com>
2017-04-21 15:51:51 -0400

    paste solutions into solutions.rb

--- solution.rb
+++ solution.rb
@@ -1,1 +1,60 @@
-# Please copy/paste all three classes into this file to submit your solution!
+class Movie
+  attr_accessor :title
+
+  ALL = []
+
+  def self.all
+    ALL
+  end
+
+  def self.find_by_title(title)
+    self.all.find do |movie|
+      movie.title == title
+    end
+  end
+
+  def initialize(title)
+    self.title = title
+    self.class.all << self
+  end
+
+  def ratings
+    Rating.all.select do |rating|
+      rating.movie == self
+    end
+  end
+
+  def viewers
+    ratings.collect do |rating|
+      rating.viewer
+    end
+  end
+
+  def average_rating
+    ratings_scores = ratings.collect do |rating|
+      rating.score
+    end
+    sum_of_all_scores = ratings_scores.inject do |sum, score|
+      sum + score
+    end
+    sum_of_all_scores.to_f / ratings_scores.size
+  end
+end
+
+class Rating
+  attr_accessor :score
+  attr_reader :movie, :viewer
+
+  ALL = []
+
+  def self.all
+    ALL
+  end
+
+  def initialize(score, movie, viewer)
+    @score = score
+    @movie = movie
+    @viewer = viewer
+    self.class.all << self
+  end
+end
